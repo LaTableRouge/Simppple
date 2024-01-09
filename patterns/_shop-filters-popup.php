10877ee2f8c4a225f6d7ce167ab9a1726cbd6645
MLNOP <victordianamlnop@gmail.com>
2024-01-09 10:49:33 +0100

    feat(woocomerce): style single wip

---
+++ patterns/_shop-filters-popup.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Title: Modale de filres
+ * Slug: simppple/shop-filters-popup
+ * Categories: simppple-sections
+ * Keywords: filters, modal
+ * Block Types: core/navigation
+ */
+
+function pattern_filters_popup_icon($html, $block) {
+    if (
+        'core/navigation' === $block['blockName']
+        && isset($block['attrs']['className'])
+        && strpos($block['attrs']['className'], 'pattern---shop-filters-popup') !== false
+    ) {
+        // Replace labels
+        $parsedHTML = new WP_HTML_Tag_Processor($html);
+        if ($parsedHTML->next_tag(['tag_name' => 'button', 'class_name' => 'wp-block-navigation__responsive-container-open'])) {
+            $parsedHTML->set_attribute('aria-label', esc_attr__('Open filters', 'simppple'));
+        }
+
+        if ($parsedHTML->next_tag(['tag_name' => 'div', 'class_name' => 'wp-block-navigation__responsive-container'])) {
+            if ($parsedHTML->next_tag(['tag_name' => 'div', 'class_name' => 'wp-block-navigation__responsive-close'])) {
+                if ($parsedHTML->next_tag(['tag_name' => 'div', 'class_name' => 'wp-block-navigation__responsive-dialog'])) {
+                    if ($parsedHTML->next_tag(['tag_name' => 'button', 'class_name' => 'wp-block-navigation__responsive-container-close'])) {
+                        $parsedHTML->set_attribute('aria-label', esc_attr__('Close filters', 'simppple'));
+                    }
+                }
+            }
+        }
+        $html = $parsedHTML->get_updated_html();
+
+        // Replace the icon if the icon option is used
+        $icon = '><span class="icon-simppple-filter" role="img" aria-label="' . esc_attr__('Filter', 'simppple') . '"></span><span>' . __('Filter', 'simppple') . '</span>';
+        $html = str_replace(
+            '><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><rect x="4" y="7.5" width="16" height="1.5" /><rect x="4" y="15" width="16" height="1.5" /></svg>',
+            $icon,
+            $html
+        );
+        $html = str_replace(
+            '><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 5v1.5h14V5H5zm0 7.8h14v-1.5H5v1.5zM5 19h14v-1.5H5V19z" /></svg>',
+            $icon,
+            $html
+        );
+
+        // Replace the menu text, if the text option is used
+        $html = str_replace('>' . __('Filters', 'simppple'), $icon, $html);
+    }
+
+    return $html;
+}
+add_filter('render_block', 'pattern_filters_popup_icon', 10, 2);
+
+?>
+
+<!-- wp:navigation {"icon":"menu","className":"pattern---shop-filters-popup","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left"}} -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:woocommerce/filter-wrapper {"filterType":"price-filter","heading":"Filter by price"} -->
+	<div class="wp-block-woocommerce-filter-wrapper">
+		<!-- wp:heading {"level":3} -->
+		<h3 class="wp-block-heading">Filter by price</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:woocommerce/price-filter {"inlineInput":true,"heading":""} -->
+		<div class="wp-block-woocommerce-price-filter is-loading"><span
+				aria-hidden="true"
+				class="wc-block-product-categories__placeholder"
+			></span></div>
+		<!-- /wp:woocommerce/price-filter -->
+	</div>
+	<!-- /wp:woocommerce/filter-wrapper -->
+
+	<!-- wp:woocommerce/filter-wrapper {"filterType":"attribute-filter","heading":"Filter by attribute"} -->
+	<div class="wp-block-woocommerce-filter-wrapper">
+		<!-- wp:heading {"level":3} -->
+		<h3 class="wp-block-heading">Filter by color</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:woocommerce/attribute-filter {"attributeId":1,"heading":"","lock":{"remove":true}} -->
+		<div class="wp-block-woocommerce-attribute-filter is-loading"></div>
+		<!-- /wp:woocommerce/attribute-filter -->
+	</div>
+	<!-- /wp:woocommerce/filter-wrapper -->
+
+	<!-- wp:woocommerce/filter-wrapper {"filterType":"attribute-filter","heading":"Filter by attribute"} -->
+	<div class="wp-block-woocommerce-filter-wrapper">
+		<!-- wp:heading {"level":3} -->
+		<h3 class="wp-block-heading">Filter by size</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:woocommerce/attribute-filter {"attributeId":2,"heading":"","lock":{"remove":true}} -->
+		<div class="wp-block-woocommerce-attribute-filter is-loading"></div>
+		<!-- /wp:woocommerce/attribute-filter -->
+	</div>
+	<!-- /wp:woocommerce/filter-wrapper -->
+</div>
+<!-- /wp:group -->
+<!-- /wp:navigation -->
---
+++ patterns/wc-breadcrumbs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Woocommerce breadcrumbs
+ * Slug: simppple/wc-breadcrumbs
+ * Categories: simppple-sections
+ * Keywords: woocommerce, breadcrumbs
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"color":{"background":"hsla(var(--wp--custom--accent-hsl), 0.2)"},"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}}},"className":"pattern---wc-breadcrumbs","layout":{"type":"constrained"}} -->
+<div
+	class="wp-block-group alignfull pattern---wc-breadcrumbs has-background"
+	style="background-color:hsla(var(--wp--custom--accent-hsl), 0.2);padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)"
+>
+	<!-- wp:group {"layout":{"type":"default"}} -->
+	<div class="wp-block-group"><!-- wp:woocommerce/breadcrumbs {"textColor":"contrast","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"},":hover":{"color":{"text":"var:preset|color|contrast"}}}}}} /--></div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
