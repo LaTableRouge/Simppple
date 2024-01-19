8a46a16ad159317ac984e3737c745433dd1797a5
MLNOP <victordianamlnop@gmail.com>
2024-01-19 09:45:43 +0100

    feat(woocommerce): order confirmation style ok

---
+++ patterns/wc-order-confirmation-totals.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Order additional informations
+ * Slug: simppple/wc-order-confirmation-totals
+ * Inserter: no
+ */
+?>
+
+<!-- wp:woocommerce/order-confirmation-totals-wrapper {"align":""} -->
+	<!-- wp:heading -->
+	<h2 class="wp-block-heading"><?php _e('Order details', 'simppple'); ?></h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:woocommerce/order-confirmation-totals {"lock":{"remove":true}} /-->
+<!-- /wp:woocommerce/order-confirmation-totals-wrapper -->
