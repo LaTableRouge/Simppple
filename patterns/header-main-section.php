403ccb7a48dd9409fc9d5935584b0d2226d92de4
MLNOP <victordianamlnop@gmail.com>
2024-10-08 11:06:30 +0200

    feat(colors): block table & calendar & latest post & file dark mode ok

--- patterns/header-main-section.php
+++ patterns/header-main-section.php
@@ -7,14 +7,14 @@
  */
 
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|10","top":"var:preset|spacing|10","right":"0","left":"0"},"blockGap":"0"}},"backgroundColor":"white","className":"pattern\u002d\u002d-header-main-section","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|10","top":"var:preset|spacing|10","right":"0","left":"0"},"blockGap":"0"}},"backgroundColor":"base","className":"pattern\u002d\u002d-header-main-section","layout":{"type":"flex","justifyContent":"space-between"}} -->
 <div
-    class="wp-block-group pattern---header-main-section has-white-background-color has-background"
+    class="wp-block-group pattern---header-main-section has-base-background-color has-background"
     style="padding-top:var(--wp--preset--spacing--10);padding-right:0;padding-bottom:var(--wp--preset--spacing--10);padding-left:0"
 >
     <!-- wp:site-title {"level":0} /-->
 
-    <!-- wp:navigation {"textColor":"contrast","icon":"menu","overlayBackgroundColor":"white","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"fontSize":"20"} /-->
+    <!-- wp:navigation {"textColor":"contrast","icon":"menu","overlayBackgroundColor":"base","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"fontSize":"20"} /-->
 </div>
 <!-- /wp:group -->
 
